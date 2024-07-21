e1d121bf1106108c03b865e565f39202ed991686
qfaxas <148553536+qfaxas@users.noreply.github.com>
2024-07-21 16:14:58 -0300

    atualização de fases

---
+++ scripts/autoload.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var fase1 = false
+var fase2 = false
+var fase3 = false
+var fase4 = false
+var fase5 = false
--- scripts/menu_principal.gd
+++ scripts/menu_principal.gd
@@ -3,7 +3,7 @@ extends Control
 
 
 func _on_play_button_pressed():
-	get_tree().change_scene_to_file("res://cenas/cena_2.tscn")
+	get_tree().change_scene_to_file("res://cenas/fase_1.tscn")
 
 
 func _on_options_button_pressed():
