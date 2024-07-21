6e85cc4ed3b18f583c7b1283e15f6da5b2723080
qfaxas <148553536+qfaxas@users.noreply.github.com>
2024-07-21 02:12:15 -0300

    base do menu de opções
    n funciona ainda

---
+++ scripts/back2menu.gd
@@ -0,0 +1,14 @@
+extends Button
+
+
+
+func _on_volume_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_graphs_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_control_remap_pressed():
+	pass # Replace with function body.
