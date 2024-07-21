1a290776bcc7eec69635a84cb11d14f69aae325f
qfaxas <148553536+qfaxas@users.noreply.github.com>
2024-07-21 02:21:57 -0300

    placeholder de cena

--- scripts/menu_principal.gd
+++ scripts/menu_principal.gd
@@ -3,7 +3,7 @@ extends Control
 
 
 func _on_play_button_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://cenas/cena_2.tscn")
 
 
 func _on_options_button_pressed():
