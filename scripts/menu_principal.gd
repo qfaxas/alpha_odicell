fd4a90aaf7c8e8ad9c0707c3fc6d1084ae410d4d
qfaxas <148553536+qfaxas@users.noreply.github.com>
2024-07-21 01:59:48 -0300

    Atualização do menu principal
    O botão de sair funciona :)

--- scripts/menu_principal.gd
+++ scripts/menu_principal.gd
@@ -2,6 +2,17 @@ extends Control
 
 
 
+func _on_play_button_pressed():
+	pass # Replace with function body.
 
-func _on_button_pressed():
-	get_tree().change_scene_to_file("res://cena_2.tscn")
+
+func _on_options_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_credits_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
