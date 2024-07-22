4e614cdd1ec7306bb7d35ba5b28030510c216dc2
felcobra <fcliporage@id.uff.br>
2024-07-22 16:35:16 -0300

    bateria na camera

--- scripts/autoload.gd
+++ scripts/autoload.gd
@@ -1,14 +1,7 @@
 extends Node
 
-@onready var energia = $Energia
-
-var tempo_restante = energia.get_time_left
-
-var escuro = false
-
-func _on_timer_timeout():
-	escuro = true
-
-func comeco(valor):
-	energia.wait_time = valor
-	energia.start()
+var fase1 = false
+var fase2 = false
+var fase3 = false
+var fase4 = false
+var fase5 = false
