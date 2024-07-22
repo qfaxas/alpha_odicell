8730301a0c3fb376cf7a18490908b30e6d49d3a5
qfaxas <148553536+qfaxas@users.noreply.github.com>
2024-07-22 14:22:54 -0300

    criei pastas pra organização

--- scripts/menu_principal.gd
+++ scripts/menu_principal.gd
@@ -3,15 +3,15 @@ extends Control
 
 
 func _on_play_button_pressed():
-	get_tree().change_scene_to_file("res://cenas/fase_1.tscn")
+	get_tree().change_scene_to_file("res://cenas/fases/fase_1.tscn")
 
 
 func _on_options_button_pressed():
-	get_tree().change_scene_to_file("res://cenas/menu_opcoes.tscn")
+	get_tree().change_scene_to_file("res://cenas/menus/menu_opcoes.tscn")
 
 
 func _on_credits_button_pressed():
-	get_tree().change_scene_to_file("res://cenas/creditos.tscn")
+	get_tree().change_scene_to_file("res://cenas/menus/creditos.tscn")
 
 
 func _on_exit_button_pressed():
