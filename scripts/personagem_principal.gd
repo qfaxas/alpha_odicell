825c01cbeb795f8a4db0ac6a927cc1521930b929
qfaxas <148553536+qfaxas@users.noreply.github.com>
2024-07-22 14:27:30 -0300

    morte

--- scripts/personagem_principal.gd
+++ scripts/personagem_principal.gd
@@ -28,4 +28,4 @@ func _physics_process(delta):
 	move_and_slide()
 	
 func morrer():
-	get_tree().change_scene_to_file("res://cenas/menu_morte.tscn")
+	get_tree().change_scene_to_file("res://cenas/menus/menu_morte.tscn")
