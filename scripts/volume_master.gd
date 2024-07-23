5ed7a428e9e1221ce4b93fdac9263c3b0d38737b
qfaxas <148553536+qfaxas@users.noreply.github.com>
2024-07-23 14:43:38 -0300

    suporte para controle 2

--- scripts/volume_master.gd
+++ scripts/volume_master.gd
@@ -8,6 +8,7 @@ var bus_index: int
 func _ready() -> void:
 	bus_index = AudioServer.get_bus_index(bus_name)
 	value_changed.connect(_on_value_changed)
+	grab_focus()
 	
 func _on_value_changed(value:float) -> void:
 	AudioServer.set_bus_volume_db(
