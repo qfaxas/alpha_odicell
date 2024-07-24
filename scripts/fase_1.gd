21720d7167fa4372dbb84b8623242894fd63a304
qfaxas <148553536+qfaxas@users.noreply.github.com>
2024-07-23 23:15:42 -0300

    -bugs

--- scripts/fase_1.gd
+++ scripts/fase_1.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Autoload.comeco(15)
+	Autoload.comeco(120)
 	
 func _process(delta):
 	Autoload.tempo_restante()
