3297e483c84505179609cc4d088b88f9e311cf92
qfaxas <148553536+qfaxas@users.noreply.github.com>
2024-07-24 17:06:48 -0300

    movimentação

--- scripts/personagem_principal.gd
+++ scripts/personagem_principal.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 const SPEED = 700.0
 const JUMP_VELOCITY = -550.0
 const Autoload = preload("res://scripts/autoload.gd")
+@onready var sprite_2d = $Sprite2D
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -13,7 +15,13 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		sprite_2d.animation = "fall"
 
+	#animações
+	if (velocity.x != 0):
+		sprite_2d.animation = "Run"
+	else:
+		sprite_2d.animation = "Idle"
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -28,7 +36,13 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
-	#checa o timer
+	#vira o personagem
+	var esquerda = velocity.x < 0
+	sprite_2d.flip_h = esquerda
+	if Input.is_action_just_pressed("left"):
+		sprite_2d.flip_h = true
+	if Input.is_action_just_pressed("right"):
+		sprite_2d.flip_h = false
 	
 
 	move_and_slide()
