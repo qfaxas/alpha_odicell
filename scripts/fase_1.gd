ebc5ff7205c46116d82a1becbc0c3a584b11f136
qfaxas <148553536+qfaxas@users.noreply.github.com>
2024-07-25 17:04:17 -0300

    mudanças no display do jogo

--- scripts/fase_1.gd
+++ scripts/fase_1.gd
@@ -3,6 +3,6 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Autoload.comeco(120)
+	get_viewport().size = DisplayServer.screen_get_size()
 	
 
--- scripts/menu_principal.gd
+++ scripts/menu_principal.gd
@@ -1,6 +1,9 @@
 extends Control
 
 
+func _ready():
+	get_viewport().size = DisplayServer.screen_get_size()
+
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://cenas/fase_1.tscn")
