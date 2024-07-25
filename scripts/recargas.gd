79d875ba30c344444d01bd9ea207831e30326abf
qfaxas <148553536+qfaxas@users.noreply.github.com>
2024-07-25 18:10:21 -0300

    recarga funcionando

--- scripts/recargas.gd
+++ scripts/recargas.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-const PersonagemPrincipal = preload("res://scripts/personagem_principal.gd")
 
 func _on_body_entered(body):
 	if (body.name == "Personagem_principal"):
-		get_tree().personagem_principal.recarga()
+		body.recarga()
+		queue_free()
