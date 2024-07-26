4c6e474a6257b026b2337a2022240f9f173caa9b
qfaxas <148553536+qfaxas@users.noreply.github.com>
2024-07-26 17:00:12 -0300

    portas

---
+++ scripts/porta.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@onready var intrans = $intrans
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_body_exited(body):
+	pass # Replace with function body.
