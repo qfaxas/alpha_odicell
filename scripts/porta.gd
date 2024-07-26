65b4cb44827770f9d11110c32fcdd6bf9b1efab7
qfaxas <148553536+qfaxas@users.noreply.github.com>
2024-07-26 17:33:22 -0300

    testes

--- scripts/porta.gd
+++ scripts/porta.gd
@@ -1,17 +1,29 @@
 extends Area2D
 
 @onready var intrans = $intrans
+@onready var label = $Label
+@onready var fechado = $fechado
+@onready var aberto = $aberto
 
+const PersonagemPrincipal = preload("res://scripts/personagem_principal.gd")
 
+var dentro :bool = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
+	if Input.is_action_just_pressed("interagir") and dentro:
+			intrans.collision_layer = 0
+			fechado.hide()
+			aberto.show()
+			PersonagemPrincipal.bateria -= 1
 
 func _on_body_entered(body):
-	pass # Replace with function body.
+	if (body.name == "Personagem_principal"):
+		label.show()
+		dentro = true
+		
 
 
 func _on_body_exited(body):
-	pass # Replace with function body.
+	if (body.name == "Personagem_principal"):
+		label.hide()
+		dentro = false
