ee50202c725197556891f6713e04c65b3e46fe3a
qfaxas <148553536+qfaxas@users.noreply.github.com>
2024-07-28 14:04:12 -0300

    consertei transiçao

--- scripts/mudar_fase.gd
+++ scripts/mudar_fase.gd
@@ -5,4 +5,5 @@ extends Area2D
 
 func _on_body_entered(body):
 	if (body.name == "Personagem_principal"):
+		Autoload.ultimo_checkpoint = null
 		get_tree().change_scene_to_file("res://cenas/fase_2.tscn")
